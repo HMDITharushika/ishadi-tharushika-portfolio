aae403c2a6bfee69b3cf9686c5839e406226eba1
ishadiIFSolutions <76896498+HMDITharushika@users.noreply.github.com>
2024-11-09 23:15:54 +0530

    Project Covers

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,7 @@
-import project1 from "../assets/projects/project-1.jpeg";
-import project2 from "../assets/projects/project-2.jpeg";
-import project3 from "../assets/projects/project-3.jpeg";
 import portfolio from "../assets/projects/portfolio.png";
 import aerotech from "../assets/projects/aero-tech.png";
+import medica from "../assets/projects/medica.png";
+import fanatismo from "../assets/projects/fanatismo.png";
 
 export const HERO_CONTENT = `I'm a dedicated software developer with a strong foundation in frontend development and a passion for crafting seamless user experiences. Proficient in React, JavaScript, and modern CSS frameworks like MUI and Tailwind CSS, I bring designs to life with clean, efficient code. My experience includes working on dynamic projects, from developing mental health tracking apps to creating artist-focused platforms. With a solid grasp of Agile methodologies and a commitment to continuous learning, I thrive in collaborative environments, always eager to tackle new challenges. `;
 
@@ -60,7 +59,7 @@ export const PROJECTS = [
   },
   {
     title: "Medica - Mental Health Status Tracking App for Cardiac Patients",
-    image: project1,
+    image: medica,
     description:
       "Medica is a mental health status tracking application designed for cardiac patients who have been in hospitals for extended periods. It allows relevant doctors to monitor and manage the mental health of these patients while they receive care for their physical health.",
     technologies: ["HTML", "MUI", "React", "Flask", "MongoDB"],
@@ -68,7 +67,7 @@ export const PROJECTS = [
   },
   {
     title: "Fanatismo - To read, post and know about your favourite artists",
-    image: project2,
+    image: fanatismo,
     description:
       "An application for uploading and reading posts and articles, featuring tools for creating and managing content.",
     technologies: ["HTML", "Bootstrap", "React"],
